0bda9bd5791c3f3a4a04836e9fa3806caa9a34d0
Pawan Kumar <pawan4super30@gmail.com>
2024-01-17 13:39:04 +0530

    Added Heading for the App

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import PageContainer from './components/PageContainer';
 function App() {
   const [elements, setElements] = useState([]);
   return (
-    <div className="w-full h-[100vh] flex justify-between">
-      <PageContainer elements={elements} setElements={setElements}/>
-      <Sidebar elements={elements} />
-    </div>
+    <>
+      <h1 className='text-center text-[40px] font-bold text-rose-600'>Mini Page Builder</h1>
+      <div className="w-full h-[100vh] flex justify-between">
+        <PageContainer elements={elements} setElements={setElements}/>
+        <Sidebar elements={elements} />
+      </div>
+    </>
   );
 }
 
